1b2ae52e50bbaf3c43b99b3b3901b06045bce3de
Matěj Chalk <matejchalk@gmail.com>
2024-02-18 19:24:10 +0100

    Render grid of icons

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,18 +1,22 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import { dirname, join } from 'node:path';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { MATERIAL_ICONS } from '../lib';
+
+const dirname = fileURLToPath(path.dirname(import.meta.url));
 
 const app = express();
 
 app.use('/icons', express.static('generated/icons'));
+app.use('/styles', express.static(path.join(dirname, 'styles')));
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', join(fileURLToPath(dirname(import.meta.url)), 'views'));
+app.set('views', path.join(dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', { icons: MATERIAL_ICONS });
 });
 
 const port = 3000;
