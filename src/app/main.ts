4684bdc92308f9e7100393fbae17f17698b76563
Matěj Chalk <matejchalk@gmail.com>
2024-02-18 10:12:25 +0100

    Integrate Handlebars and PicoCSS with Express server

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const app = express();
 
 app.use('/icons', express.static('generated/icons'));
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', join(fileURLToPath(dirname(import.meta.url)), 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 const port = 3000;
 app.listen(port, () => {
