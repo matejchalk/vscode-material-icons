c5ae5ac45d9a2fee95462040ba6219fc0c3c3c41
Vojtech Masek <vojtech@flowup.cz>
2024-12-06 11:39:31 +0100

    sort material icons array by name alphabetically

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import iconMap from './generated/icon-map.json' with { type: 'json' };
 import type { MaterialIcon } from './types.js';
 
-export const MATERIAL_ICONS = Object.keys(iconMap.iconDefinitions) as [
+export const MATERIAL_ICONS = Object.keys(iconMap.iconDefinitions).sort() as [
   MaterialIcon,
   MaterialIcon,
   ...MaterialIcon[],
